9e00ed890892e955aab039fbc8207f58275a6299
Jorge Sauceda <jorge@jsauceda.info>
2022-12-30 22:50:54 -0600

    Update portfolio files

--- dist/main.js
+++ dist/main.js
@@ -6,13 +6,3 @@ const toggleTheme = (lightTheme) => {
     document.documentElement.classList.remove("dark");
   }
 };
-
-// responsive navBar and logo elements
-const menuButton = document.getElementById("menu-button");
-const navElements = document.getElementById("nav-list");
-const logo = document.getElementById("logo");
-
-menuButton.addEventListener("click", () => {
-  navElements.classList.toggle("show");
-  logo.classList.toggle("display");
-});
