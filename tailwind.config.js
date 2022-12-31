9a7da8d7e92d925c204f8ab3aa2e3bedc09ab79b
Jorge Sauceda <jorge@jsauceda.info>
2022-12-30 22:52:18 -0600

    Update portfolio files

--- tailwind.config.js
+++ tailwind.config.js
@@ -4,13 +4,7 @@ module.exports = {
   content: ["./*.html"],
   theme: {
     extend: {
-      colors: {
-        brightRed: "hsl(12, 88%, 59%)",
-        brightRedLight: "hsl(12, 88%, 69%)",
-        darkBlue: "hsl(228, 39%, 23%)",
-        darkGrayBlue: "hsl(227, 12%, 61%)",
-        lightGray: "hsl(0, 0%, 98%)",
-      },
+      colors: {},
     },
   },
   plugins: [require("@tailwindcss/forms")],
